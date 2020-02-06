596e23b874fe04859901849fce45e1026af324bd
Row35 <ivan.shtoyko@gmail.com>
2020-02-06 13:46:09 +0200

    HW_1 added

---
+++ 1HW/userbot.py
@@ -0,0 +1,11 @@
+"""
+Hello, (answer(name))“, “Your age is  (answer(age))“, “You live in  (answer(city)) 
+"""
+user_name = (input("What is your name? "))
+user_age = (input("How old are you? "))
+user_location = (input("Where do you live? "))
+# while user_age == str : print ("input your age by numbers!") 
+#else : 
+print ("Your age is,",user_age)
+print ("Hello,",user_name)
+print ("You live in,",user_location)
