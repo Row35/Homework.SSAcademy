1874636617431e189b94516b39bc4a602eb202c0
Row35 <ivan.shtoyko@gmail.com>
2020-02-17 21:58:52 +0200

    cw up to 130220 were updated

---
+++ CW/CW_110220.py
@@ -0,0 +1,56 @@
+# maxormin=int(input('enter the element qantity of your list = '))
+# f=[]
+# for i in range(maxormin):
+#     f.append(int(input('enter next element of your list, step by step = ')))
+# # for m in f
+# #     if 
+# print("max val of ur list =", min(f))
+# print("max val of ur list =", max(f))
+
+# #2 
+# even=[]
+# odd=[]
+# els=[]
+# for i in range(10):
+#     if i%2==0:
+#         even.append(i)
+#     elif i%3%2==0:
+#         odd.append(i)
+#     else:
+#         els.append(i)
+# print(f'ur list of even {even} \n odd {odd} \n else numers {els}')
+
+# # 3 factorial
+# a=int(input("insert a = "))
+# fct=1
+# for i in range(a): 
+#     fct = fct * (i+1) 
+# print (f"The factorial of your number is : {fct}",end="") 
+
+# "Напишіть скрипт, який перевіряє логін, який вводить користувач. 
+# login=input("insert your login = ")
+# while login != str('First'):
+#     login=input("Login incorrect!!! Insert your login again = ")
+#     continue
+# else:
+#     print(f"Welcome, {login}")
+    
+
+# a=int(input('enter the element qantity of your list = '))
+# f=[]
+# for i in range(a):
+#     f.append(int(input('enter next element of your list, step by step = ')))
+# # for m in f
+# #     if 
+# print("max val of ur list =", min(f))
+# print("max val of ur list =", max(f))
+
+
+# Написати програму, яка буде зчитувати числа поки не зустріне від’ємне число. При появі від’ємного числа програма зупиняється (якщо зустрічається 0 програма теж зупиняється).
+# int_list=[]
+# n = int(input("Please enter the first element: "))
+# while n > 0:
+#     int_list.append(n)
+#     n = int(input("Please enter next element: "))
+# else:
+#     print(int_list)
